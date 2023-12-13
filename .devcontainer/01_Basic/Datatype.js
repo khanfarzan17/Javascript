9d8225d69bca96862443b6151b2370693192cdcc
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-12-13 06:40:59 +0000

    Datatypes

---
+++ .devcontainer/01_Basic/Datatype.js
@@ -0,0 +1,18 @@
+/* Datatypes in Javascript */
+
+let name="Farzan"
+let age=23
+let isloggedIn=false
+console.log(name)
+console.table([age,isloggedIn])
+
+/* Null is a datatype also and a standalone value 
+//object//
+
+//string => ""
+//boolean=>True/False//
+undefined= value is not being assigned to that //
+symbol is also a datatype which is being used to assign unique componets  example used in figma//
+*/
+
+
