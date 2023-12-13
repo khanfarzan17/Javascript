791c4591feed5de6b3c23885cfc3ebebe9185de6
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-12-13 05:59:08 +0000

    Variables

---
+++ .devcontainer/01_Basic/variables.js
@@ -0,0 +1,17 @@
+const accountId=121
+let accountEmail="khanfarzan@gmail.com"
+var accountPassword=12345
+accountCity="Bangalore"
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+/*
+Prefer not to use Var 
+Because in the issue in Block Scope and Functional Scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
