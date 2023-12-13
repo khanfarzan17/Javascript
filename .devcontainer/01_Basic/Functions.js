b6a610743bb7c7cf31a69fa64435b4cb5b0832b0
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-12-13 07:10:13 +0000

    Converstions

---
+++ .devcontainer/01_Basic/Functions.js
@@ -0,0 +1,25 @@
+//let score="22abc"//
+//let score="33"//
+// let score=null
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// we make sure the Datatype of the score should be Number not a string///
+// let value=Number(score)
+// console.log(typeof value)
+// console.log(value)
+
+
+/* If we give the score value undefined then the output will be NaN 
+If we give the score value as True/ False then it will give Either 1 or 0 
+If we give numeric value then the output will be Number
+If we give numeric with Double Quotes then it is String 
+If we give numeric with character then it will give NaN
+*/
+
+let isloggedIn=1
+
+let boolean=Boolean(typeof Boolean)
+
+console.log(boolean);
+// here Boolean is the Datatype which cheack whether it is Getting Converted or not///
