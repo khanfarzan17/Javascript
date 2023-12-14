066c9174a559294787c0cf318c0958253f0a1bef
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-12-14 07:10:35 +0000

    Strings

---
+++ .devcontainer/String/stringBasic.js
@@ -0,0 +1,8 @@
+const name="Farzan";
+const Repository=50;
+
+console.log(`my name is ${name} in my Githb i have  ${Repository} Repository `);
+
+
+const comapnayName=new String('SLK Software ');
+console.log(comapnayName);
