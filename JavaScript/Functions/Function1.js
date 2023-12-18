49642db1fdd8a070606fc2bdc20b709105e7c48f
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-12-18 07:42:37 +0000

    Functions

---
+++ JavaScript/Functions/Function1.js
@@ -0,0 +1,48 @@
+/// Adding 2 number //
+function addNum(number1,number2,sum){
+  number1=12;
+   number2=12;
+  sum=number1+number2
+  console.log(sum)
+}
+addNum();
+
+//  Multiplication of 2 number//
+function multiply(number3,number4){
+    console.log(number3*number4);
+}
+multiply(10,4);
+
+// otherWay to Dcalre and Define a function//
+
+function substract(number5,number6){
+    let result= (number5-number6);
+    return result;
+}
+
+const result=substract(15,5);
+console.log("Result", result);
+
+// Rules in Function //
+// after return we cannot print any thing///
+
+
+function loginUser(username){
+    return `${username} is logged in as User`
+}
+console.log(loginUser("Farzan"))
+console.log(loginUser())// Undefined is logged in as user//
+
+
+/// If in fucntion//
+
+function signIn(username){
+    if(username===undefined){
+        console.log("Please enter username ")
+        return
+
+    }
+    return `${username} is Signinned`
+}
+
+console.log(signIn())
