3e85e13dc68d9fdf5dfed98c041c985fb5e3a0ee
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-12-20 09:33:51 +0000

    foreach

---
+++ JavaScript/ControlStatements/ForEachLoop.js
@@ -0,0 +1,58 @@
+// Foreach  is being used  for Call Back Function//
+let myArray=[12,3,4,56,7]
+
+myArray.forEach(function(myArray){
+    //console.log(myArray)// in call Back function Function dont have its Name//
+})
+
+
+
+// let SuperHero=["flash","Batman","Superman","Hulk"]
+
+// SuperHero.forEach(function(rou){
+// console.log(rou)
+// })
+
+let anuj=["34","Hello ","How are you"]
+
+anuj.forEach(item => console.log(item))// Arrow Function IN FOR EACH LOOP//
+
+//////////////// Using forEach by uaing its refernce in javaScript //////////////////////////
+
+let alok=["Satyam ", "Intekhab","Tarique","Shourya"]
+
+// alok.forEach(item => console.log(item))
+console.log("-===========================================================")
+
+/// now by using refernce//
+
+function pgMates(how){
+console.log(how);
+}
+alok.forEach(pgMates)
+
+///////////////////////// How acces in objects in JSON Format/////////////////////
+
+let Myoffice=[
+
+    {
+        SlKclient:"Emerson ",
+        SlkTeam:" EMR"
+    },
+    {
+        SlKclient:"Fifth Third Bank",
+        SlkTeam:"five3"
+    },
+    {
+        SlKclient:"Regions Bank ",
+        SlkTeam:"CBU Buffer"
+    }
+]
+
+Myoffice.forEach(function(item){
+    console.log(item.SlKclient)// normal Function //
+})
+Myoffice.forEach((item)=>{
+    console.log(item.SlkTeam)// Arrow Function  in javScript//
+    
+})
